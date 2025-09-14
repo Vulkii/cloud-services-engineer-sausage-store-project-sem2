a4c805c88940de48821978d7c8ff541b3122b918
Matvei Sukhikh <Vulkii@yandex.ru>
2025-09-14 23:14:52 +0300

    fix source code

--- backend/src/main/java/com/yandex/practicum/devops/model/Product.java
+++ backend/src/main/java/com/yandex/practicum/devops/model/Product.java
@@ -1,9 +1,11 @@
 package com.yandex.practicum.devops.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Product {
 
     @Id
