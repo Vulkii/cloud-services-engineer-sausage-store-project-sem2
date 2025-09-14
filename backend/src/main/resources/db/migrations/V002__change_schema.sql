1f422825656697488c7543f5ba5e9ed8c06882d2
Matvei Sukhikh <Vulkii@yandex.ru>
2025-09-14 19:47:44 +0300

    Fixed DockerFiles and added migrations

---
+++ backend/src/main/resources/db/migrations/V002__change_schema.sql
@@ -0,0 +1,21 @@
+ALTER TABLE product
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE orders
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE product
+    ADD price DOUBLE PRECISION;
+
+ALTER TABLE orders
+    ADD date_created date;
+
+DROP TABLE product_info;
+
+DROP TABLE orders_date;
+
+ALTER TABLE order_product
+    ADD CONSTRAINT fk_orders_id FOREIGN KEY (order_id) REFERENCES orders (id);
+
+ALTER TABLE order_product
+    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product (id);
